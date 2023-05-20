75cc2a48bea503b023ccf50b37a290660d3cf79b
Andrew <anqwertyp@gmail.com>
2023-05-20 23:27:01 +0300

    *

---
+++ LearnFS/API_LearnFS/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiLearnfsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'API_LearnFS'
---
+++ LearnFS/GAME_LearnFS/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GameLearnfsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GAME_LearnFS'
