6aafb1878bd6f0bc7da63f2f61ae81608e708688
Andrew <anqwertyp@gmail.com>
2023-05-21 22:54:39 +0300

    Добавлен новый поддомен (ln)

--- LearnFS/LearnFS/hosts.py
+++ LearnFS/LearnFS/hosts.py
@@ -2,6 +2,6 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('',
     host(r'api', 'API_LearnFS.urls', name='api'),
-    host(r'clientǀwwwǀ ', 'CLIENT_LearnFS.urls', name='client'),
+    host(r'clientǀlnǀwwwǀ ', 'CLIENT_LearnFS.urls', name='client'),
     host(r'game', 'GAME_LearnFS.urls', name='game'),
 )
