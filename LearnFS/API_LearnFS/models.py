15af38fc042c5cbe6c09b5c1c77b59dd94d43953
Andrew <anqwertyp@gmail.com>
2023-05-22 18:47:52 +0300

    Создана база данныз для API

--- LearnFS/API_LearnFS/models.py
+++ LearnFS/API_LearnFS/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    group = models.CharField(max_length=5)
+    
+    def __str__(self):           
+        return self.name
+    
+class Subject(models.Model):
+    name = models.CharField(max_length=35)
+    groups = models.ManyToManyField(Group)
+    
+    def __str__(self):          
+        return self.name
+    
+class Theme(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    subs_clases = models.ForeignKey('Subject', on_delete=models.PROTECT)
+    
+    def __str__(self):          
+        return self.name
+    
+class Summary(models.Model):
+    body = models.TextField()
+    themes = models.ForeignKey("Theme",on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.name
+
+class Test(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    themes = models.ForeignKey("Theme",on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.name
+    
+class Question(models.Model):
+    text = models.TextField()
+    tests = models.ForeignKey("Test",on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.name
+    
+class Ans_Que(models.Model):
+    answer = models.CharField(max_length=255)
+    isRight = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
