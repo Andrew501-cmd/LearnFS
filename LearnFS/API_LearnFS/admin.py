d1e0046b461bf3bded2a418294a2547b3655e40f
Andrew <anqwertyp@gmail.com>
2023-05-24 00:03:25 +0300

    Добавлены таблицы в админ-панель

--- LearnFS/API_LearnFS/admin.py
+++ LearnFS/API_LearnFS/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Ans_Que)
+admin.site.register(Group)
+admin.site.register(Subject)
+admin.site.register(Theme)
+admin.site.register(Test)
+admin.site.register(Summary)
+admin.site.register(Question)
