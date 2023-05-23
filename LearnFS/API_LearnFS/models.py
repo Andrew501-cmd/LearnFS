3865fe91e989fca7f826a38131f0f0f5b96caf68
Andrew <anqwertyp@gmail.com>
2023-05-24 00:26:10 +0300

    Добавлены русские названия для таблиц (БД) в админ-панель

--- LearnFS/API_LearnFS/models.py
+++ LearnFS/API_LearnFS/models.py
@@ -5,12 +5,20 @@ from django.db import models
 class Group(models.Model):
     group = models.CharField(max_length=5)
     
+    class Meta:
+        verbose_name = 'Класс'
+        verbose_name_plural = 'Классы'
+    
     def __str__(self):           
         return self.name
     
 class Subject(models.Model):
     name = models.CharField(max_length=35)
     groups = models.ManyToManyField(Group)
+    
+    class Meta:
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
     
     def __str__(self):          
         return self.name
@@ -20,12 +28,20 @@ class Theme(models.Model):
     description = models.CharField(max_length=255)
     subs_clases = models.ForeignKey('Subject', on_delete=models.PROTECT)
     
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+    
     def __str__(self):          
         return self.name
     
 class Summary(models.Model):
     body = models.TextField()
     themes = models.ForeignKey("Theme",on_delete=models.PROTECT)
+    
+    class Meta:
+        verbose_name = 'Конспект'
+        verbose_name_plural = 'Конспекты'
     
     def __str__(self):
         return self.name
@@ -35,6 +51,10 @@ class Test(models.Model):
     description = models.CharField(max_length=255)
     themes = models.ForeignKey("Theme",on_delete=models.PROTECT)
     
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+    
     def __str__(self):
         return self.name
     
@@ -42,12 +62,20 @@ class Question(models.Model):
     text = models.TextField()
     tests = models.ForeignKey("Test",on_delete=models.PROTECT)
     
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+    
     def __str__(self):
         return self.name
     
 class Ans_Que(models.Model):
     answer = models.CharField(max_length=255)
     isRight = models.BooleanField(default=False)
+    
+    class Meta:
+        verbose_name = 'Ответ на вопрос'
+        verbose_name_plural = 'Ответы на вопросы'
     
     def __str__(self):
         return self.name
